c3befc114b6647a8f7bb7253ccb522d6f2a4ee90
istekharr Ali <istekaar83@gmail.com>
2025-03-22 15:06:39 +0530

    user controoler

--- controllers/user.js
+++ controllers/user.js
@@ -3,6 +3,7 @@ import jwt from "jsonwebtoken";
 import {asyncHendler} from "../utils/asyncHeandler.js";
 import {ApiResponse} from "../utils/ApiResponse.js";
 import {ApiError} from "../utils/ApiError.js";
+import e from "cors";
 
 const generateAcessAndRefreshToken = async(userId)=>{
     try {
@@ -53,6 +54,64 @@ const userRegister=asyncHendler(async(req, res)=>{
     .json(new ApiResponse(200, createUser, "user register successfully"))
 })
 
+const loginUser=asyncHendler(async(req, res)=>{
+    const {email, password}=req.body
+
+    if(!(email && password)){
+        throw new ApiError(400, "eamil and password are required")
+    }
+
+    const user=await User.findOne(email)
+    
+    if(!user){
+        throw new ApiError(404, "user not found")
+    }
+    
+    const isPasswordCurret=await user.isPasswordCurrect(password)
+    if(!isPasswordCurret){
+        throw new ApiError(404, "password wrong")
+    }
+    
+    const {accessToken, refreshToken} = generateAcessAndRefreshToken(user?.id)
+
+    const logedinUser=await User.findOne(user.id).select("-password -refreshToken")
+
+    const options={
+        httpOnly: true,
+        secure: true
+    }
+
+    return res
+    .status(200)
+    .cookie("accessToken", accessToken, options)
+    .cookie("refreshToken", refreshToken, options)
+    .json(200, {user:logedinUser, accessToken, refreshToken}, "login successfully")
+})
+
+const logoutUser=asyncHendler(async(req, res)=>{
+    await User.findByIdAndUpdate(
+        req.user._id,
+        {
+            $unset:{
+                refreshToken: 1
+            }
+        },
+        {new: true}
+    )
+
+    const options={
+        httpOnly: true,
+        secure: true
+    }
+
+    return res
+    .status(200)
+    .clearCookie("accessToken", options)
+    .clearCookie("refreshToken", options)
+    .json(new ApiResponse(200, {}, "logout successfully"))
+})
 export{
     userRegister,
+    loginUser,
+    logoutUser,
 }
